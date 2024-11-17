2c04e093ace6de946f6bfc5e6c86f8a1f4a4a046
n0o0b <168068514+n00o00b@users.noreply.github.com>
2024-11-17 22:31:59 +0800

    Add files via upload

---
+++ reverse/avoid_me/docker/service/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Get the user
+user=$(ls /home)
+
+# Check the environment variables for the flag and assign to INSERT_FLAG
+if [ "$DASFLAG" ]; then
+    INSERT_FLAG="$DASFLAG"
+elif [ "$FLAG" ]; then
+    INSERT_FLAG="$FLAG"
+elif [ "$GZCTF_FLAG" ]; then
+    INSERT_FLAG="$GZCTF_FLAG"
+else
+    INSERT_FLAG="flag{TEST_Dynamic_FLAG}"
+fi
+
+# 将FLAG写入文件 请根据需要修改
+echo $INSERT_FLAG | tee /home/$user/flag
+
+# 赋予程序运行权限
+chmod 777 /home/ctf/attachment
+chmod 777 /home/ctf/avoid_me
+
+
+/etc/init.d/xinetd start;
+sleep infinity;
